512ed40fba1e9cd91f5f2573fd2d4ab7d17e336f
rafafsi <rafaelafsinacio@gmail.com>
2022-02-07 21:05:41 -0300

    criação das classes Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso1 Java");
+        curso1.setDescricao("descrição curso1");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso2 JavaScript");
+        curso2.setDescricao("descrição curso2");
+        curso2.setCargaHoraria(4);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria de java");
+        mentoria1.setDescricao("descricão mentoria java");
+        mentoria1.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
+
+//instancio o novo objeto curso1 da classe Curso com new.
